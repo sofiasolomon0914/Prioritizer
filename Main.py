2a044105976f249ad0e7f40276488e1b05c0c58e
Sofia Solomon <ssolomon2770>
2019-11-21 17:25:24 -0500

    Commit 1

---
+++ Main.py
@@ -0,0 +1,50 @@
+"""
+
+This is my main file for this program
+__author__ = Sofia Solomon
+This program will help people prioritize their activities and interests,
+hopefully leaving them more relaxed and organized.
+
+"""
+
+# Print welcome statement
+print("\nHello, Welcome to Prioritizer! This program will help you organize "
+      "\nall of your responsibilities, activities, and interests. This "
+      "\nprogram is ideal for people who hate having their lives controlled "
+      "by "
+      "\nrigid schedules and planners but at the same time like feeling "
+      "\norganized. This program will first ask you questions concerning you "
+      "\nresponsibilities, involvements, and interests. It will then go one "
+      "to "
+      "\nask you about upcoming appointments and commitments. Finally, "
+      "\nthis program will present all the information you entered but in an "
+      "\norganized format that you can hopefully use for your benefit. "
+      "Enjoy! ")
+
+"""
+First, ask the user what their major responsibilities are. The user may only 
+input three activities. Save these activities as variables.
+"""
+responsibility_first = str(input("\nThink of your major responsibilities. "
+                                 "This "
+                                 "can include work, school,\nfamily "
+                                 "obligations, ect. You will only be aloud "
+                                 "to enter three, so choose wisely.\nEnter "
+                                 "your first major responsibility: "))
+responsibility_second = str(input("\nEnter a second major responsibility: "))
+responsibility_third = str(input("\nEnter your third major responsibility: "))
+
+# Things you find most interesting or relaxing or fun: may only include
+# four(can be a sport, hobbies, interest, ect)
+
+# Things that you want to pursue: can include infinite amounts (a project
+# you want to start, a book you want to read, hobbies you want to start, ect)
+
+# Small things for this coming week. May include appointments, ect.
+
+# Important dates this coming month.
+
+# Important dates this coming year
+
+
+# Output prioritize will print
